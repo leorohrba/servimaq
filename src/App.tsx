1b726b473f83a231d0385626204f2d709402eec0
leo <leorohrbacher@gmail.com>
2025-06-24 16:28:26 -0300

    Helmet fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/home/";
 import AboutPage from "./components/about/";
 import ProductsPage from "./components/products/";
-import { Helmet } from 'react-helmet-async';
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import ContactPage from "./components/contact";
 // import theme from './components/themed'; // Assuming you have a theme file
 import { system } from "./components/theme"; // Importing the theme
@@ -13,25 +13,29 @@ import { system } from "./components/theme"; // Importing the theme
 function App() {
   return (
     <ChakraProvider value={system}>
-      <Helmet>
-        <title>ServiMaq | Aluguel de equipamentos para construção civil</title>
-        <meta
-          name="description"
-          content="Há mais de 10 anos locando equipamentos com agilidade e confiança para obras em Joinville e região."
-        />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-745470874"
-        ></script>
-        <script>
-          {`
+      <HelmetProvider>
+        <Helmet>
+          <title>
+            ServiMaq | Aluguel de equipamentos para construção civil
+          </title>
+          <meta
+            name="description"
+            content="Há mais de 10 anos locando equipamentos com agilidade e confiança para obras em Joinville e região."
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-745470874"
+          ></script>
+          <script>
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-745470874');
-          `}
-        </script>
-      </Helmet>
+            `}
+          </script>
+        </Helmet>
+      </HelmetProvider>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
